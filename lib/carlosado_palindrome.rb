e456533c852690b2a5b8d845d2b6d5dca9d4ace2
carlosado <diaoliveira@gmail.com>
2019-04-15 12:30:51 +0100

    Initialize repository

---
+++ lib/carlosado_palindrome.rb
@@ -0,0 +1,6 @@
+require "carlosado_palindrome/version"
+
+module CarlosadoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/carlosado_palindrome/version.rb
@@ -0,0 +1,3 @@
+module CarlosadoPalindrome
+  VERSION = "0.1.0"
+end
